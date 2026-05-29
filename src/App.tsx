1ba72a19c82db1cbbf1202d6d50fcec3bf9a43f7
kodatinikhil <kodativinnu@gmail.com>
2026-05-29 16:28:49 +0530

    fix color

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { TotalRewardsTable } from "./components/rewards/TotalRewardsTable";
 
 import { Loader } from "./components/common/Loader";
 import { ErrorMessage } from "./components/common/ErrorMessage";
-import { appStyles } from "../src/styles/appStyles";
 import "./App.css";
 
 function App() {
@@ -26,22 +25,22 @@ function App() {
     return <ErrorMessage message={error} />;
   }
 
- return (
-  <div style={appStyles.container}>
-    <h1 style={appStyles.title}>Rewards Program Dashboard</h1>
+return (
+  <div className="container">
+    <h1>Rewards Program Dashboard</h1>
 
-    <div style={appStyles.card}>
-      <h2 style={appStyles.sectionTitle}>Transactions</h2>
+    <div className="card">
+      <h2>Transactions</h2>
       <TransactionsTable transactions={transactions} />
     </div>
 
-    <div style={appStyles.card}>
-      <h2 style={appStyles.sectionTitle}>Monthly Rewards</h2>
+    <div className="card">
+      <h2>Monthly Rewards</h2>
       <MonthlyRewardsTable monthlyRewards={monthlyRewards} />
     </div>
 
-    <div style={appStyles.card}>
-      <h2 style={appStyles.sectionTitle}>Total Rewards</h2>
+    <div className="card">
+      <h2>Total Rewards</h2>
       <TotalRewardsTable totalRewards={totalRewards} />
     </div>
   </div>
